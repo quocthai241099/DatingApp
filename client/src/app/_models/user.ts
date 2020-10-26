b046a5ea6dedb574ee4afa6fdc9346d4b54c54dc
quocthai241099 <65201872+quocthai241099@users.noreply.github.com>
2020-10-26 13:46:02 +0700

    End of section 6

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -1,5 +1,5 @@
 export interface User
 {
-    usernam: string;
+    username: string;
     token: string;
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { error } from 'protractor';
 import { Observable } from 'rxjs';
 import { threadId } from 'worker_threads';
@@ -13,7 +15,7 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     
@@ -22,17 +24,19 @@ export class NavComponent implements OnInit {
   {
     this.accountService.login(this.model).subscribe(response => 
       {
-        console.log(response)
+        this.router.navigateByUrl('/members');
         
       }, error => {
         console.log(error);
+        this.toastr.error(error.error);
+        
       }
       );
   }
   logout()
   {
     this.accountService.logout();
-    
+    this.router.navigateByUrl('/');
   }
   
 
