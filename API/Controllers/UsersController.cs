92af9242f57ebb22b8f13845cc9c8eeb5100b0d1
quocthai241099 <65201872+quocthai241099@users.noreply.github.com>
2020-10-29 11:43:38 +0700

    End of section 10

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace API.Controllers
@@ -35,6 +36,18 @@ namespace API.Controllers
         public async Task<ActionResult<MemberDto>> GetUser(string username)
         {
             return await userRepository.GetMemberAsync(username);
+            
+        }
+        [HttpPut]
+        public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await userRepository.GetUserByUsernameAsync(username);
+
+            mapper.Map(memberUpdateDto, user);
+            userRepository.Update(user);
+            if(await userRepository.SaveAllAsync()) return NoContent();
+            return BadRequest("Failed to update user");
             
         }
     }
--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient} from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { NavigationExtras, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
 
 
 
@@ -12,16 +13,36 @@ import { NavigationExtras, Router } from '@angular/router';
 })
 export class MembersService {
   baseUrl = 'https://localhost:5001/api/';
-
+  members: Member[] = [];
   constructor(private http: HttpClient) { }
 
   getMembers()
   {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members.length > 0) return of(this.members);
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+      
+    )
   }
+
   getMember(username: string)
   {
+    const member = this.members.find(x => x.username === username);
+    if (member !== undefined) return of(member);
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
+  }
+
+  updateMember(member: Member)
+  {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = member;
+      })
+    )
   }
  
 }
